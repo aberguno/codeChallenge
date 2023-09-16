d5a31fbaa6c5c23c238d6779aaff7856a3403568
Ariel Berguño <ariel.berguno@bazaarvoice.com>
2023-09-16 17:20:09 +0200

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Ariel Berguño on 16/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
